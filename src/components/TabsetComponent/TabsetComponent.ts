ac5849d2bf3eb0c2b3512e7051a69df705150a41
Max Andruh <maksim.andrux@gmail.com>
2018-09-07 17:55:59 +0300

    minor changes

--- src/components/TabsetComponent/TabsetComponent.ts
+++ src/components/TabsetComponent/TabsetComponent.ts
@@ -9,11 +9,6 @@ export default {
     TabComponent,
   },
   props: [],
-  // data () {
-  //   return {
-  //     tabsets: []
-  //   }
-  // },
   computed: {
      ...mapGetters([
        'fullTabsetsData',
@@ -22,8 +17,6 @@ export default {
   mounted() {
     // @ts-ignore
     this.$store.dispatch(UPLOAD_NEW_TABSET, 'tabsetsData');
-    // @ts-ignore
-    return this.$store.state.tabsets
   },
   methods: {
   }
