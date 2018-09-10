74b4b08eee543dd46adfd466ea14c85c1e6d84a7
roma <romaboyko2017@gmail.com>
2018-09-10 23:21:18 +0300

    added close icon on hover

--- src/components/TabComponent/TabComponent.ts
+++ src/components/TabComponent/TabComponent.ts
@@ -1,20 +1,25 @@
 export default {
-  name: 'TabComponent',
-  components: {},
-  props: {
-    tab: Object
-  },
-  data () {
-    return {
-
+    name: 'TabComponent',
+    components: {},
+    props: {
+        tab: Object
+    },
+    data() {
+        return {
+            hovered: false as boolean
+        }
+    },
+    computed: {},
+    mounted() {
+    },
+    methods: {
+        hover() {
+            //@ts-ignore
+            this.hovered = true
+        },
+        unHover() {
+            //@ts-ignore
+            this.hovered = false
+        }
     }
-  },
-  computed: {
-
-  },
-  mounted () {
-  },
-  methods: {
-
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
-import devtools from './../node_modules/@vue/devtools'
+// import devtools from './../node_modules/@vue/devtools'
 
 import App from './App.vue';
 import router from './router';	
@@ -12,7 +12,7 @@ import { FETCH_TABSETS_DATA, UPLOAD_NEW_TABSET } from '@/store/actions.type';
 Vue.use(VueMaterial);
 Vue.config.productionTip = true;
 Vue.config.devtools = true;
-devtools.connect("http://localhost", "8098");
+// devtools.connect("http://localhost", "8098");
 
 new Vue({
   router,
