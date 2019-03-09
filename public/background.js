560f4a519f68e64479968046624d486c09f1548d
Mivnv <maksim.andrux@gmail.com>
2019-03-09 16:34:36 +0200

    refactoring

--- public/background.js
+++ public/background.js
@@ -1,104 +1,104 @@
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB;
 
 const fetchChromeTabs = () => {
-	return new Promise(resolve => {
-		chrome.tabs.query({}, tabs => {
-			resolve(tabs);
-		});
-	});
+  return new Promise(resolve => {
+    chrome.tabs.query({}, tabs => {
+      resolve(tabs);
+    });
+  });
 };
 
-const openConnection = (dbName) => {
-	return new Promise(resolve => {
-		const request = indexedDB.open(dbName, 1);
-		let db = null;
-		console.log(request);
-		request.onsuccess = () => {
-			if (request.result.objectStoreNames.length)
-				resolve(request.result);
-		};
-		request.onupgradeneeded = (event) => {
-			db = event.target;
-			console.log(db);
-			const objectStore = db.result.createObjectStore('tabsets', {
-				keyPath: 'id'
-			});
+const openConnection = dbName => {
+  return new Promise(resolve => {
+    const request = indexedDB.open(dbName, 1);
+    let db = null;
+    console.log(request);
+    request.onsuccess = () => {
+      if (request.result.objectStoreNames.length) resolve(request.result);
+    };
+    request.onupgradeneeded = event => {
+      db = event.target;
+      console.log(db);
+      const objectStore = db.result.createObjectStore("tabsets", {
+        keyPath: "id"
+      });
 
-			objectStore.onsuccess = () => {				
-				resolve(db.result);
-			};
-		};
-	});
+      objectStore.onsuccess = () => {
+        resolve(db.result);
+      };
+    };
+  });
 };
 
-const getObjectStore = (db) => {
-	if (db) return db.transaction('tabsets', 'readwrite').objectStore('tabsets')
-	else return 'error';
+const getObjectStore = db => {
+  if (db) return db.transaction("tabsets", "readwrite").objectStore("tabsets");
+  else return "error";
 };
 
 const createNewTab = () => {
-	return new Promise(resolve => {
-		chrome.tabs.create({
-			'url': chrome.extension.getURL('index.html')
-		}, (newTab) => {
-			resolve(newTab);
-		});
-	});
-}
+  return new Promise(resolve => {
+    chrome.tabs.create(
+      {
+        url: chrome.extension.getURL("index.html")
+      },
+      newTab => {
+        resolve(newTab);
+      }
+    );
+  });
+};
 
-const receiveMessage = (response) => {
-	console.log(response);
-	chrome.runtime.onMessage.addListener(
-		(request, sender, sendResponse) => {
-			if (request.type == 'fetchClosedTabset' && response) {
-				sendResponse(response);
-				response = null;
-			}
-		});
+const receiveMessage = response => {
+  console.log(response);
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type == "fetchClosedTabset" && response) {
+      sendResponse(response);
+      response = null;
+    }
+  });
 };
 
 const closeTabs = (tabs, newTab) => {
-	let processedTabs = [];
-
-	for (let tab of tabs) {
-		let notCurrentTab = newTab.id !== tab.id;
-		let notNewTab = tab.url.indexOf('chrome://newtab') === -1;
-		let notExtPage = tab.url.indexOf(newTab.url) === -1;
-
-		if (notCurrentTab && notNewTab && notExtPage) {
-			processedTabs.push(tab);
-			//chrome.tabs.remove(tab.id);
-		}
-	}
-	return processedTabs;
+  const processedTabs = tabs.filter(tab => {
+    const notCurrentTab = newTab.id !== tab.id;
+    const notNewTab = tab.url.indexOf("chrome://newtab") === -1;
+    const notExtPage = tab.url.indexOf(newTab.url) === -1;
+    return notCurrentTab && notNewTab && notExtPage && !tab.pinned;
+  });
+  //processedTabs.map(tab => chrome.tabs.remove(tab.id));
+  return processedTabs;
 };
 
 const uploadTabsetToIndexedDB = (store, tabset) => {
-	const createdAt = new Date();
-	const processedTabset = {
-		id: +createdAt,
-		tabs: tabset,
-		locked: false,
-		createdAt: createdAt,
-		tabsetName: '',
-		stared: false
-	};
+  const createdAt = new Date();
+  const processedTabset = {
+    id: +createdAt,
+    tabs: tabset,
+    locked: false,
+    createdAt: createdAt,
+    tabsetName: "",
+    stared: false
+  };
 
-	const req = store.put(processedTabset);
+  const req = store.put(processedTabset);
 
-	return new Promise(resolve => {		
-		req.onsuccess = () => resolve(processedTabset);	
-	});
+  return new Promise(resolve => {
+    req.onsuccess = () => resolve(processedTabset);
+  });
 };
 
 const fetchClosedTabset = async () => {
-	const tabs = await fetchChromeTabs();
-	const newTab = await createNewTab();
-	const closedTabset = closeTabs(tabs, newTab);
-	const db = await openConnection('tabsetsData');
-	const store = getObjectStore(db);
-	const processedTabset = await uploadTabsetToIndexedDB(store, closedTabset);
-	receiveMessage(processedTabset);
+  const tabs = await fetchChromeTabs();
+  const newTab = await createNewTab();
+  const closedTabset = closeTabs(tabs, newTab);
+  const db = await openConnection("tabsetsData");
+  const store = getObjectStore(db);
+  const processedTabset = await uploadTabsetToIndexedDB(store, closedTabset);
+  receiveMessage(processedTabset);
 };
 
 chrome.browserAction.onClicked.addListener(fetchClosedTabset);
--- src/containers/TheNavigationBarComponent/TheNavigationBarComponent.ts
+++ src/containers/TheNavigationBarComponent/TheNavigationBarComponent.ts
@@ -1,44 +1,37 @@
-import {
-  SORT_BY_STAR,
-} from '@/store/actions.type';
-import {mapGetters} from "vuex";
-import {SHOW_ONLY_STARED} from "@/store/mutations.type";
-
+import { SORT_BY_STAR } from "@/store/actions.type";
+import { mapGetters } from "vuex";
+import { SHOW_ONLY_STARED } from "@/store/mutations.type";
 
 export default {
-  name: 'TheNavigationBarComponent',
+  name: "TheNavigationBarComponent",
   components: {},
   props: [],
-  
-  data () {
-    return { 
+
+  data() {
+    return {
       menuVisible: false,
       count: 0
-    }
+    };
   },
 
   computed: {
-    ...mapGetters([
-      'showOnlyStared',
-      'fullTabsetsData'
-    ]),
+    ...mapGetters(["showOnlyStared", "fullTabsetsData"])
   },
 
   mounted() {
     setTimeout(() => {
-      console.log('SADAS',  this.fullTabsetsData)
+      console.log("SADAS", this.fullTabsetsData);
       for (let tabset of this.fullTabsetsData) {
         if (tabset.stared) {
-          this.count++
+          this.count++;
         }
       }
-    }, 500);
-
+    }, 500); 
   },
   methods: {
-     toggleShowOnlyStared() {
+    toggleShowOnlyStared() {
       this.$store.commit(SHOW_ONLY_STARED, !this.showOnlyStared);
       this.$store.dispatch(SORT_BY_STAR);
     }
   }
-}
+};
