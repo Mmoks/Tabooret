76942a7320f26779736040be62b9d14a26ec9f47
Mivnv <maksim.andrux@gmail.com>
2019-03-09 22:59:29 +0200

    code refatoring

--- src/containers/TabsetsListComponent/TabsetsListComponent.vue
+++ src/containers/TabsetsListComponent/TabsetsListComponent.vue
@@ -1,4 +1,12 @@
-<template src="./TabsetsListComponent.html"></template>
+<template>
+  <div class="md-layout md-gutter md-alignment-top-center">
+    <md-list v-if="fullTabsetsData.length">
+      <div v-for="tabset in fullTabsetsData" :key="tabset.id">
+        <TabsetComponent v-if="tabset && tabset.show" :tabset="tabset"/>
+      </div>
+    </md-list>
+  </div>
+</template>
 <script src="./TabsetsListComponent.ts" lang="ts"></script>
 <style src="./TabsetsListComponent.stylus" scoped lang="stylus"></style>
 
