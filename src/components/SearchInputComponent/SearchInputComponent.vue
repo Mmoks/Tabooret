2bde80bc9f57230e7f301be451ad2eac3af640aa
Mivnv <maksim.andrux@gmail.com>
2019-03-09 22:59:29 +0200

    code refatoring

--- src/components/SearchInputComponent/SearchInputComponent.vue
+++ src/components/SearchInputComponent/SearchInputComponent.vue
@@ -1,4 +1,17 @@
-<template src="./SearchInputComponent.html"></template>
+<template>
+  <md-field class="search__input_container md-transparent">
+    <i
+      :class="{ search__input_icon_focused: isFocused }"
+      class="search__input_icon material-icons"
+    >search</i>
+    <md-input
+      @blur="isFocused = false"
+      @focus="isFocused = true"
+      class="search__input"
+      placeholder="Search items"
+    ></md-input>
+  </md-field>
+</template>
 <script src="./SearchInputComponent.ts" lang="ts"></script>
 <style src="./SearchInputComponent.stylus" scoped lang="stylus"></style>
 
--- src/components/TabsetComponent/TabsetComponent.vue
+++ src/components/TabsetComponent/TabsetComponent.vue
@@ -1,3 +1,63 @@
-<template src="./TabsetComponent.html"></template>
+<template>
+  <md-list-item>
+    <md-content
+      md-with-hover
+      class="tabset__content md-elevation-2"
+      :class="{ 'md-elevation-6': isHovered }"
+      @mouseenter="isHovered = true"
+      @mouseleave="isHovered = false"
+    >
+      <md-list class="tabs__list">
+        <div style="height: 60px; padding-top:5px">
+          <span
+            class="md-title tabset__name md-list-item-text"
+            :class="{ hide__tabset__title: nameIsEditing }"
+            @click.prevent="startEditingTabsetName"
+          >{{ tabset.tabsetName || tabset.tabs.length + ' tabs' }}</span>
+
+          <md-field
+            md-inline
+            :md-counter="false"
+            class="tabsetname__input"
+            :class="{ hide__input: !nameIsEditing }"
+          >
+            <md-input
+              ref="tabsetNameInput"
+              maxlength="50"
+              v-model="tabsetName"
+              @blur.native="saveTabsetName"
+              @keyup.esc="cancelEditing"
+              @keyup.enter="saveTabsetName"
+              v-autowidth="{maxWidth: '960px', minWidth: '20px', comfortZone: 0}"
+            ></md-input>
+          </md-field>
+
+          <span
+            class="md-caption created__at md-list-item-text"
+          >Created at {{ tabset.createdAt.toLocaleString() }}</span>
+          <i
+            class="material-icons un__stared"
+            :class="{'stared': tabset.stared}"
+            @click="toggleStar"
+          >{{ tabset.stared ? "star_rate" : "star_border" }}</i>
+          <i
+            class="md-raised lock__open material-icons"
+            @click="toggleLock"
+          >{{ tabset.locked ? "lock" : "lock_open" }}</i>
+        </div>
+        <md-divider></md-divider>
+        <TabComponent
+          v-for="tab in tabset.tabs"
+          :key="tab.id"
+          :lockedTabset="tabset.locked"
+          :tab="tab"
+          :tabsetID="tabset.id"
+        />
+        <!-- 			<div class="control__button">
+        <md-button class="md-raised md-primary" v-bind:disabled="tabset.locked">Restore tabset</md-button>-->
+      </md-list>
+    </md-content>
+  </md-list-item>
+</template>
 <script src="./TabsetComponent.ts" lang="ts"></script>
 <style src="./TabsetComponent.stylus" scoped lang="stylus"></style>
